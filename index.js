bc2871857f2bd369990abe5480e173e9bd876a47
liangruuu <1024311844@qq.com>
2019-03-28 22:09:49 +0800

    2-2 什么是模块打包工具？

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import Header from './header.js'
-import Content from './content.js'
-import Sidebar from './sidebar.js'
+// ES Module 模块引入方式
+// CommonJS 模块引入方式
+// CMD, AMD...
 
+// webpack 模块打包工具
+
+// import Header from './header.js'
+// import Content from './content.js'
+// import Sidebar from './sidebar.js'
+
+let Header = require('./header.js')
+let Content = require('./content.js')
+let Sidebar = require('./sidebar.js')
 
 // let header = document.createElement('div')
 // header.innerText = 'header'
